f639fabb283d566245328f54f508cbf85bd005bd
Stattek <stattek1@pm.me>
2026-08-23 11:54:02 -0500

    Fix movie compilation and format

--- procgame/dmd/movie.py
+++ procgame/dmd/movie.py
@@ -18,52 +18,55 @@ import zipfile
 try:
     import cv2
     from pkg_resources import parse_version
-    OPCV3 = parse_version(cv2.__version__) >= parse_version('3')
+
+    OPCV3 = parse_version(cv2.__version__) >= parse_version("3")
 except Exception, e:
-    logging.error("OpenCV is not available on your system.  The Movie (mp4) support is unavailable")
+    logging.error(
+        "OpenCV is not available on your system.  The Movie (mp4) support is unavailable"
+    )
+
 
 # returns OpenCV VideoCapture property id given, e.g., "FPS"
 def capPropId(prop):
-  return getattr(cv2 if OPCV3 else cv2.cv,
-    ("" if OPCV3 else "CV_") + "CAP_PROP_" + prop)
+    return getattr(
+        cv2 if OPCV3 else cv2.cv, ("" if OPCV3 else "CV_") + "CAP_PROP_" + prop
+    )
+
 
 def getColorProp():
-  return cv2.cv.CV_BGR2RGB if not OPCV3 else cv2.COLOR_BGR2RGB
+    return cv2.cv.CV_BGR2RGB if not OPCV3 else cv2.COLOR_BGR2RGB
+
 
 class Movie(object):
     """An ordered collection of :class:`~procgame.dmd.Frame` objects."""
-    
+
     width = None
     """Width of each of the animation frames in dots."""
     height = None
     """Height of each of the animation frames in dots."""
     frames = None
     """Ordered collection of :class:`~procgame.dmd.Frame` objects."""
-    vc=None
-    
-    
-    
+    vc = None
+
     def __init__(self, filename):
         """Initializes the animation."""
         super(Movie, self).__init__()
 
-        if(cv2 is None):
-            raise ValueError, "MP4 is unavailable as OpenCV is not installed"
-        
+        if cv2 is None:
+            raise ValueError("MP4 is unavailable as OpenCV is not installed")
+
         self.vc = cv2.VideoCapture(filename)
         self.width = int(self.vc.get(capPropId("FRAME_WIDTH")))
         self.height = int(self.vc.get(capPropId("FRAME_HEIGHT")))
-        self.frame_count  = int(self.vc.get(capPropId("FRAME_COUNT")))
-        if self.width==0 or self.height==0:
-            raise ValueError, "Movie failed to load filename: '%s'" % filename
+        self.frame_count = int(self.vc.get(capPropId("FRAME_COUNT")))
+        if self.width == 0 or self.height == 0:
+            raise ValueError("Movie failed to load filename: '%s'" % filename)
 
-        #print 'ARE WE SET TO RGB?'
-        #print self.vc.get(cv.CV_CAP_PROP_CONVERT_RGB)
-        #self.vc.set(cv.CV_CAP_PROP_CONVERT_RGB, True)
-        #print self.vc.get(cv.CV_CAP_PROP_CONVERT_RGB)
+        # print 'ARE WE SET TO RGB?'
+        # print self.vc.get(cv.CV_CAP_PROP_CONVERT_RGB)
+        # self.vc.set(cv.CV_CAP_PROP_CONVERT_RGB, True)
+        # print self.vc.get(cv.CV_CAP_PROP_CONVERT_RGB)
 
     def __del__(self):
         if self.vc != None:
             self.vc.release()
-        
-
